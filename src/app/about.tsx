a759abb185d35cafc0a2eb5c9ca7e9fc1e5e8cb9
Josh Heng <josh@joshheng.co.uk>
2024-04-03 17:53:05 +0100

    :sparkles: Update about section

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,24 +1,33 @@
+import { FiExternalLink } from 'react-icons/fi';
+
 export default function About() {
   return (
     <section className="-top-8 relative mb-4">
       <h2>What is WSAF?</h2>
-      <p>
-        Warwick Student Arts Festival (WSAF) is a 3 day showcase and celebration
-        of all aspects of the arts here at Warwick!
-      </p>
-      <p>
-        Every day will feature a programme of events including theatre, dance,
-        music, film, literature, comedy and art held across the entirety of
-        campus.
-      </p>
-      <p>
-        Performances will run throughout the day, so there will be things to see
-        and do from the morning until the evening.
-      </p>
-      <p>
-        If you have a show, some art, a dance, a poem or anything in between
-        that you want to show, take part!
-      </p>
+      <div className="px-8 text-lg max-w-5xl mx-auto">
+        <p className="font-bold mb-2">
+          Warwick Student Arts Festival (WSAF) is a 3 day showcase and
+          celebration of all aspects of the arts here at Warwick!
+        </p>
+        <p>
+          Every day will feature a morning-to-evening programme of events
+          including theatre, dance, music, film, literature, comedy and art held
+          across the entirety of campus. If you have a show, some art, a dance,
+          a poem or anything in-between that you want to show, we'd love to see
+          you there!
+        </p>
+        <a
+          href="https://forms.office.com/pages/responsepage.aspx?id=vc-6Ce9HZUSSZTVG8ur2vFkUeDcXxJtJj843aI4gzYJUNEJZSVlJN1k5UlBKUE01VzA2VjhNMlIyMiQlQCN0PWcu"
+          target="_blank"
+          className="inline-block bg-secondary m-4 px-4 py-1 rounded-sm drop-shadow-sm hover:scale-105"
+        >
+          <span className="text-2xl uppercase font-bold">
+            Registration Form Now Open{' '}
+            <FiExternalLink className="inline-block mb-2" />
+          </span>
+          <span className="block text-sm">Closes Friday 26th April</span>
+        </a>
+      </div>
     </section>
   );
 }
