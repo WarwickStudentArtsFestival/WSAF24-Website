9186b2ada883dc802d64903f28d72cec551a72b6
Josh Heng <josh@joshheng.co.uk>
2024-04-06 18:16:26 +0100

    :wrench: Add redirects to submission form and instagram

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  redirects: () => ([
+    {
+      source: '/instagram',
+      destination: 'https://www.instagram.com/wsaf24/',
+      permanent: true,
+    },
+    {
+      source: '/submit',
+      destination: 'https://forms.office.com/e/VCDf8eLLTp',
+      permanent: true,
+    },
+    {
+      source: '/submissions',
+      destination: 'https://forms.office.com/e/VCDf8eLLTp',
+      permanent: true,
+    }
+  ])
 };
 
 export default nextConfig;
