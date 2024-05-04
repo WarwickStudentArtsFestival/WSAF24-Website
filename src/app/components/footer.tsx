7a61830259b1ef4eb9b2799bad88548cf8dd029d
Josh Heng <josh@joshheng.co.uk>
2024-05-05 00:17:56 +0100

    :sparkles: Add Warwick SU and Warwick Presents logo to footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import warwickTechCrewLogo from '@/assets/organisations/warwick-tech-crew.png';
+import warwickSuLogo from '@/assets/organisations/warwick-su.svg';
+import warwickPresentsLogo from '@/assets/organisations/warwick-presents.png';
 
 export default function Footer() {
   return (
@@ -10,12 +12,29 @@ export default function Footer() {
           WSAF would not be possible without the generous support of our
           delivery partners:
         </p>
-        <div className="flex justify-center gap-4 mt-1">
+        <div className="flex justify-center items-center gap-4 mt-1">
           <a href="https://www.warwicktechcrew.co.uk/" target="_blank">
             <Image
               src={warwickTechCrewLogo}
               alt="Warwick Tech Crew logo"
-              className="h-16 w-auto"
+              className="h-12 sm:h-16 w-auto"
+            />
+          </a>
+          <a
+            href="https://warwick.ac.uk/students/warwickpresents/"
+            target="_blank"
+          >
+            <Image
+              src={warwickPresentsLogo}
+              alt="Warwick Presents logo"
+              className="h-8 sm:h-10 w-auto"
+            />
+          </a>
+          <a href="https://www.warwicksu.com/" target="_blank">
+            <Image
+              src={warwickSuLogo}
+              alt="Warwick SU logo"
+              className="h-9 sm:h-14 w-auto"
             />
           </a>
         </div>
