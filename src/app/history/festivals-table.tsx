38d438229149d0b9ea79c18605ad1b09ad2504fa
Josh Heng <josh@joshheng.co.uk>
2024-05-20 00:42:45 +0100

    :speech_balloon: Work on WSAF history

---
+++ src/app/history/festivals-table.tsx
@@ -0,0 +1,213 @@
+export default function FestivalsTable() {
+  return (
+    <table className="mx-auto">
+      <tr>
+        <th>Name</th>
+        <th>Dates</th>
+        <th>Events</th>
+        <th>Links</th>
+        <th>Comments</th>
+      </tr>
+      <tr>
+        <th>WSAF</th>
+        <td>20-24 June 2004</td>
+        <td>50+</td>
+        <td>
+          <a
+            href="https://web.archive.org/web/20050506042341/http://www.wsaf.org.uk:80/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF &apos;05</th>
+        <td>19-23 June 2005</td>
+        <td></td>
+        <td>
+          <a
+            href="https://web.archive.org/web/20210918050951/https://blogs.warwick.ac.uk/wsaf/?num=10&start=10"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Warwick Blog
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF &apos;06</th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF &apos;07</th>
+        <td>24-28 June 2007</td>
+        <td>77+</td>
+        <td>
+          <a
+            href="https://web.archive.org/web/20070630063134/http://www.wsaf.org.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF &apos;08</th>
+        <td>22-26 June 2008</td>
+        <td>65+</td>
+        <td>
+          <a
+            href="https://web.archive.org/web/20081007200600/http://www.wsaf.org.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>SPLAT-Fest</th>
+        <td>21-25 June 2009</td>
+        <td>Unknown</td>
+        <td>
+          <a
+            href="http://web.archive.org/web/20090524043909/http://wsaf.org.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+          <br />
+          <a
+            href="https://warwick.ac.uk/newsandevents/pressreleases/award-winning_author_to/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Press Release
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF 2010</th>
+        <td>25-28 June 2010</td>
+        <td>100+</td>
+        <td>
+          <a
+            href="https://warwick.ac.uk/newsandevents/news-old/wsaf2010"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            News Article
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF 2011</th>
+        <td>25-28 June 2011</td>
+        <td>Unknown</td>
+        <td>
+          <a
+            href=" http://web.archive.org/web/20110628003050/http://www.wsaf.co.uk/about.php"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+          <br />
+          <a
+            href="https://warwick.ac.uk/newsandevents/news-old/warwick-student-arts-festival-2011"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            News Article
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF 2012</th>
+        <td>24-27 June 2012</td>
+        <td>Unknown</td>
+        <td>
+          <a
+            href="http://web.archive.org/web/20120710055823/http://www.wsaf.co.uk/whatiswsaf.php"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+          <br />
+          <a
+            href="https://warwick.ac.uk/newsandevents/news-old/wsaf_2012/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            News Article
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>WSAF 2013</td>
+        <td>22-25 June 2013</td>
+        <td>Unknown</td>
+        <td>
+          <a
+            href="http://web.archive.org/web/20130810135711/http://wsaf.co.uk/whatiswsaf.php"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Website
+          </a>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF 2024</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>WSAF 2015</th>
+        <td>21-24 June 2015</td>
+        <td>130</td>
+        <td>
+          <a
+            href="https://www.facebook.com/warwickstudentartsfest/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Facebook Page
+          </a>
+        </td>
+        <td></td>
+      </tr>
+    </table>
+  );
+}
--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,10 +1,28 @@
 import PageHeader from '@/app/components/page-header';
+import FestivalsTable from '@/app/history/festivals-table';
 
 export default function History() {
   return (
     <main>
-      <PageHeader title="History" />
-      <p>History</p>
+      <PageHeader title="The History of WSAF" />
+      <p>Bit of an introduction</p>
+
+      <section>
+        <FestivalsTable />
+      </section>
+
+      <section>
+        <p>
+          The first ever Warwick Student Arts Festival took place in 2004, with
+          over 50 events take place across campus in a spectacular celebration
+          of the creative talent at the University.
+        </p>
+
+        <p>
+          Warwick Student Arts Festival was temporarily renamed to SPLAT-Fest,
+          standing for Student Performance, Literature, Art & Theatre.
+        </p>
+      </section>
     </main>
   );
 }
